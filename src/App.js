f53e11d72c49caec58290fc88f3dc07dd00c805a
Hary Pham <haryphamdev@gmail.com>
2021-10-10 16:55:52 +0700

    video4 dynamic data in templates

--- src/App.js
+++ src/App.js
@@ -6,13 +6,22 @@ import './App.css';
 //babel
 
 const App = () => {
+  let name = 'Eric'; //string
+  let number = 2021;
+  let obj = { name: 'ERic', channel: 'Hoi Dan IT' };
+  let link = 'https://www.youtube.com/watch?v=frVHVZDpsp4&list=PLncHg6Kn2JT4xzJyhXfmJ53dzwVbq-S_E';
+
   return (
     <div className="App">
+      {console.log('check obj: ', obj)}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello world with React and Hoi Dan IT!</h1>
+        <h1>Hello world with React and {name}!</h1>
+        <p>{JSON.stringify(obj)}</p>
+        <a href={link} target="_blank"  >Visit my channel</a>
       </header>
     </div>
+
   );
 }
 
