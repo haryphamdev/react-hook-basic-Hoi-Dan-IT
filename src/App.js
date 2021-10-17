ecd32da1f250f0e238a6668ea5f77a72acbf354f
Hary Pham <haryphamdev@gmail.com>
2021-10-17 09:27:28 +0700

    video 10 print list array

--- src/App.js
+++ src/App.js
@@ -12,22 +12,45 @@ const App = () => { //class
   //state
   let [name, setName] = useState('Eric'); //[a1, b1, c1....]
   const [address, setAddress] = useState('');
+  const [todos, setTodos] = useState([
+    { id: 'todo1', title: 'Watching Hoi Dan IT Channel' },
+    { id: 'todo2', title: 'Doing homework' },
+    { id: 'todo3', title: 'Playing game' }
+  ]);
+
 
   const handleEventClick = (event) => {
-    console.log(address)
-    setName(address);
+    if (!address) {
+      alert('emtpy input')
+      return;
+    }
+    //hook not merge state
+    //...spread syntax array js
+    let newTodo = { id: 'abc', title: address }
+    setTodos([...todos, newTodo])
+    setAddress('')
   }
 
   const handleOnchangeInput = (event) => {
     setAddress(event.target.value)
   }
   //re-render
+  //for for-each => map
   return (
     <div className="App">
       <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello world with React and {name}!</h1>
+        <div className="todos-container">
+          {todos.map(todo => {
+            console.log('>>> check todo list: ', todo)
+            return (
+              <li className="todo-child" key={todo.id}> {todo.title}</li>
+            )
+          })}
+
+        </div>
         <input type="text" value={address} onChange={(event) => handleOnchangeInput(event)} />
         <button type="button" onClick={(event) => handleEventClick(event)}>Click me</button>
       </header>
