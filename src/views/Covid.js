59ffcaacdf29eb3095f8ed1e54d17c8e48ba89ce
Hary Pham <haryphamdev@gmail.com>
2021-10-20 16:15:06 +0700

    video17 react conditional message

--- src/views/Covid.js
+++ src/views/Covid.js
@@ -5,17 +5,24 @@ import moment from 'moment';
 const Covid = () => {
 
     const [dataCovid, setDataCovid] = useState([]);
+    const [loading, setLoading] = useState(true)
     // componentDidMount
     useEffect(async () => {
-        let res = await axios.get('https://api.covid19api.com/country/vietnam?from=2021-10-01T00:00:00Z&to=2021-10-20T00:00:00Z')
-        let data = res && res.data ? res.data : [];
-        if (data && data.length > 0) {
-            data.map(item => {
-                item.Date = moment(item.Date).format('DD/MM/YYYY');
-                return item;
-            })
-        }
-        setDataCovid(data)
+        setTimeout(async () => {
+            let res = await axios.get('https://api.covid19api.com/country/vietnam?from=2021-10-01T00:00:00Z&to=2021-10-20T00:00:00Z')
+            let data = (res && res.data) ? res.data : []; // true, false
+            if (data && data.length > 0) {
+                data.map(item => {
+                    item.Date = moment(item.Date).format('DD/MM/YYYY');
+                    return item;
+                })
+
+                data = data.reverse()
+            }
+            setDataCovid(data);
+            setLoading(false);
+        }, 3000)
+
     }, []);
 
     return (
@@ -33,7 +40,8 @@ const Covid = () => {
                 </thead>
                 <tbody>
 
-                    {dataCovid && dataCovid.length > 0 &&
+
+                    {loading === false && dataCovid && dataCovid.length > 0 &&
                         dataCovid.map(item => {
                             return (
                                 <tr key={item.ID}>
@@ -45,6 +53,12 @@ const Covid = () => {
                                 </tr>
                             )
                         })
+                    }
+
+                    {loading === true
+                        && <tr >
+                            <td colSpan='5' style={{ 'textAlign': 'center' }}>  Loading...</td>
+                        </tr>
                     }
 
                 </tbody>
